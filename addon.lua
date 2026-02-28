9a2f6be06deb9707f22fe48e2d20cd741d9a556f
David Lynch <kemayo@gmail.com>
2026-02-28 00:09:12 -0600

    Fix for money display over 999 gold

--- addon.lua
+++ addon.lua
@@ -17,7 +17,7 @@ local function copperToPrettyMoney(c, coins)
     end
 
     if c >= 10000 then
-        return ("|cffffffff%d|r|cffffd700%s|r |cffffffff%d|r|cffc7c7cf%s|r |cffffffff%d|r|cffeda55f%s|r"):format(
+        return ("|cffffffff%s|r|cffffd700%s|r |cffffffff%d|r|cffc7c7cf%s|r |cffffffff%d|r|cffeda55f%s|r"):format(
             BreakUpLargeNumbers(c/10000), G, (c/100)%100, S, c%100, C
         )
     elseif c >= 100 then
